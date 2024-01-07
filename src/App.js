67fe0a1a70e763150e001d665ebd57adb50a63e3
thomas <thomascaraujo19@gmail.com>
2024-01-06 21:39:14 -0300

    aprov

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const url = "http://localhost:3000/products"
 
 function App() {
   const [products, setProducts] = useState([]);
-  
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
   useEffect(() => { //Não funcionou com o async aqui
@@ -21,7 +22,32 @@ function App() {
       fetchData();
   }, []); //Não há dependencias
 
-    console.log(products)
+    // 2 - adição de produtos
+
+    const handleSubmit = async (e) => {//função que vai receber um evento (e)
+      e.preventDefault()
+
+      const product = {
+        name,
+        price,
+      };
+    
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      });
+
+      // 3 - carregamento dinâmico
+
+      const addedProduct = await res.json()
+
+      setProducts((prevProducts) => [...prevProducts, addedProduct]);
+      setName("");
+      setPrice("");
+    };
   
 
   return (
@@ -34,6 +60,25 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: 
+            <input type="text" 
+            value={name} 
+            name="name" 
+            onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço: 
+            <input type="number" 
+            value={price} 
+            name="price" 
+            onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value= "Criar" />
+        </form>
+      </div>
     </div>
   );
 }
